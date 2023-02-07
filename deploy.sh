7c3a865cadddc598675b1382aa6c194274afcc77
Jorge <83678281+krRyptt0m01@users.noreply.github.com>
2023-02-06 19:56:34 -0500

    primer commit

--- deploy.sh
+++ deploy.sh
@@ -26,6 +26,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:Zheone9/recommendation-page.git main:gh-pages
+# git push -f git@github.com:Zheone9/page-recommendation.git main:gh-pages
 
 cd -
